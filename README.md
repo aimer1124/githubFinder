c7f7721f768074826cb9a21a1bd8ba6c98df641d
aimer1124 <yjshi@thoughtworks.com>
2018-09-18 10:39:35 +0800

    All Scirpts is done.

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 ## First Step
 
 - Init Project
+
+## 
--- github.js
+++ github.js
@@ -2,15 +2,21 @@ class Github {
   constructor() {
     this.client_id = 'a9d77689ba82525e52c5';
     this.client_secret = 'bacb5847f637e15b5d7692949bdbbaa7c22d455e';
+    this.repos_count = 5;
+    this.repos_sort = 'created:asc';
   }
 
   async getUser(user) {
     const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
 
+    const reposResponse = await fetch(`https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`);
+
     const profile = await profileResponse.json();
+    const repos = await reposResponse.json();
 
     return {
-      profile
+      profile,
+      repos
     }
   }
 }
