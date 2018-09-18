6145a37d7a737fadd725d50eda853d849b834390
aimer1124 <yjshi@thoughtworks.com>
2018-09-18 10:01:34 +0800

    Add Search User

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const github = new Github;
 
+const ui = new UI;
+
 const searchUser = document.getElementById('searchUser');
 
 searchUser.addEventListener('keyup', (e) => {
@@ -8,10 +10,12 @@ searchUser.addEventListener('keyup', (e) => {
   if(userText !== '') {
     github.getUser(userText).then(data => {
       if (data.profile.message === 'Not Found') {
-        console.log(`Not Found Username:${userText}`);
+        ui.showAlert('User not found', 'alert alert-danger');
       } else {
-        console.log(data);
+        ui.showProfile(data.profile);
       }
     })
+  } else {
+    ui.clearProfile();
   }
 });
